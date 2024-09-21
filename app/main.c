ed7d210391c4caeed54fde2d9f752ced29dda6ec
rmzmrnn <marananremzell@gmail.com>
2024-09-21 20:39:45 +0800

    add while loop to not exit the program and still wait for prompt

--- app/main.c
+++ app/main.c
@@ -2,16 +2,18 @@
 #include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
+  
+  while(1){
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  input[strlen(input) - 1] = '\0'; //Removes new line
-  printf("%s: command not found", input);
-
+    input[strlen(input) - 1] = '\0'; //Removes new line
+    printf("%s: command not found", input);
+  }
+ 
   return 0;
 }
